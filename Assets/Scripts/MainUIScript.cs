be68a3e92219896229f29231b5fec76112e002a8
lizhug <lizhug.steven@gmail.com>
2021-11-12 16:02:40 +0800

    初始化

---
+++ Assets/Scripts/MainUIScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUIScript : MonoBehaviour
+{
+    public GameObject startPanel;
+    public GameObject selectPropertyPanel; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+     * 
+     * 开始 重新创建人生
+     * 
+     */
+    public void StartNewLife()
+    {
+        Debug.Log("展示人生选择UI");
+
+        startPanel.GetComponent<CanvasGroup>().alpha = 0;
+        startPanel.GetComponent<CanvasGroup>().interactable = false;
+        startPanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+        selectPropertyPanel.GetComponent<CanvasGroup>().alpha = 1;
+        selectPropertyPanel.GetComponent<CanvasGroup>().interactable = true;
+        selectPropertyPanel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
